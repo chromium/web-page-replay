76de05badb275d96e482875b153a15c908393b2c
tonyg@google.com <tonyg@google.com@164481a5-15cb-f69f-4b93-856c5b7754c5>
2010-09-24 03:38:36 +0000

    Dummynet on Windows XP
    git-svn-id: http://web-page-replay.googlecode.com/svn/trunk@35 164481a5-15cb-f69f-4b93-856c5b7754c5

--- platformsettings.py
+++ platformsettings.py
@@ -42,19 +42,46 @@ class TrafficShapingError(PlatformSettingsError):
 class PlatformSettings(object):
   def __init__(self):
     self.original_primary_dns = None
+    self.is_traffic_shaping = False
 
-  def set_traffic_shaping(self, bandwidth=0, delay_ms=0, packet_loss_rate=0):
+  def set_traffic_shaping(self, bandwidth='0', delay_ms='0',
+                          packet_loss_rate='0'):
     """Start shaping traffic.
 
     Args:
-      bandwidth: Bandwidth in [K|M]{bit/s|Byte/s}. Zero means unlimited.
-      delay_ms: Propagation delay in milliseconds. Zero means no delay.
-      packet_loss_rate: Packet loss rate in range [0..1]. Zero means no loss.
+      bandwidth: Bandwidth in [K|M]{bit/s|Byte/s}. '0' means unlimited.
+      delay_ms: Propagation delay in milliseconds. '0' means no delay.
+      packet_loss_rate: Packet loss rate in range [0..1]. '0' means no loss.
     """
-    raise NotImplementedError()
+    if self.is_traffic_shaping:
+      self.restore_traffic_shaping()
+    if bandwidth == '0' and delay_ms == '0' and packet_loss_rate == '0':
+      return
+    try:
+      # Create pipe '1' with requested shape.
+      self._ipfw([
+          'pipe', '1',
+          'config',
+          'bw', bandwidth,
+          'delay', delay_ms,
+          'plr', packet_loss_rate
+      ])
+      # Install pipe '1'.
+      self._ipfw(['add', '1', 'pipe', '1', 'src-ip', '127.0.0.1'])
+      logging.info('Started shaping traffic')
+      self.is_traffic_shaping = True
+    except:
+      raise TrafficShapingError()
 
   def restore_traffic_shaping(self):
-    raise NotImplementedError()
+    if not self.is_traffic_shaping:
+      return
+    try:
+      # Delete pipe '1' (which was created in set_traffic_shaping).
+      self._ipfw(['delete', '1'])
+      logging.info('Stopped shaping traffic')
+    except:
+      raise TrafficShapingError()
 
   def get_primary_dns(self):
     raise NotImplementedError()
@@ -68,37 +95,16 @@ class PlatformSettings(object):
     self.set_primary_dns(self.original_primary_dns)
     self.original_primary_dns = None
 
-
-class OsxAndLinuxCommonPlatformSettings(PlatformSettings):
-
-  def set_traffic_shaping(self, bandwidth=0, delay_ms=0, packet_loss_rate=0):
-    try:
-      # Create pipe '1' with requested shape.
-      subprocess.check_call([
-          'ipfw',
-          'pipe', '1',
-          'config',
-          'bw', bandwidth,
-          'delay', delay_ms,
-          'plr', packet_loss_rate
-      ])
-      # Install pipe '1'.
-      subprocess.check_call(
-          ['ipfw', 'add', '1', 'pipe', '1', 'src-ip', '127.0.0.1'])
-      logging.info('Started shaping traffic')
-    except:
-      raise TrafficShapingError()
-
-  def restore_traffic_shaping(self):
-    try:
-      # Delete pipe '1' (which was created in set_traffic_shaping).
-      subprocess.check_call(['ipfw', 'delete', '1'])
-      logging.info('Stopped shaping traffic')
-    except:
-      raise TrafficShapingError()
+  def _ipfw(self, args):
+    raise NotImplementedError()
 
 
-class OsxPlatformSettings(OsxAndLinuxCommonPlatformSettings):
+class PosixPlatformSettings(PlatformSettings):
+  def _ipfw(self, args):
+    subprocess.check_call(['ipfw'] + args)
+
+
+class OsxPlatformSettings(PosixPlatformSettings):
   def _scutil(self, cmd):
     scutil = subprocess.Popen(
         ['scutil'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -143,7 +149,7 @@ class OsxPlatformSettings(OsxAndLinuxCommonPlatformSettings):
     logging.info('Changed system DNS to %s', dns)
 
 
-class LinuxPlatformSettings(OsxAndLinuxCommonPlatformSettings):
+class LinuxPlatformSettings(PosixPlatformSettings):
   """The following thread recommends a way to update DNS on Linux:
 
   http://ubuntuforums.org/showthread.php?t=337553
@@ -196,6 +202,7 @@ class LinuxPlatformSettings(OsxAndLinuxCommonPlatformSettings):
 class WindowsPlatformSettings(PlatformSettings):
   def _netsh_dns(self, cmd, arg=''):
     try:
+      # TODO: "Local Area Connection" is a default, but not necessarily correct.
       subprocess.check_call(
           'netsh interface ip %s dns name="Local Area Connection" %s' % (
           cmd, arg))
@@ -212,12 +219,21 @@ class WindowsPlatformSettings(PlatformSettings):
     self._netsh_dns('set', 'dhcp')
 
 
+class WindowsXpPlatformSettings(WindowsPlatformSettings):
+  def _ipfw(self, args):
+    subprocess.check_call(['third_party\ipfw_win32\ipfw.exe'] + args)
+
+
 def get_platform_settings():
   system = platform.system()
+  release = platform.release()
   if system == 'Darwin':
     return OsxPlatformSettings()
   elif system == 'Linux':
     return LinuxPlatformSettings()
   elif system == 'Windows':
-    return WindowsPlatformSettings()
-  raise NotImplementedError('Sorry, %s is not yet supported.', system)
+    if release == 'XP':
+      return WindowsXpPlatformSettings()
+    else:
+      return WindowsPlatformSettings()
+  raise NotImplementedError('Sorry, %s %s is not supported.', (system, release))
