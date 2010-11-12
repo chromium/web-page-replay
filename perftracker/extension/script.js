a35612ba8b7a32e28c37d65633072eec51e29440
tonyg@google.com <tonyg@google.com@164481a5-15cb-f69f-4b93-856c5b7754c5>
2010-11-12 22:41:56 +0000

    Implement LastLoadTime. No vetting yet, time to see how well it works.
    git-svn-id: http://web-page-replay.googlecode.com/svn/trunk@118 164481a5-15cb-f69f-4b93-856c5b7754c5

--- perftracker/extension/script.js
+++ perftracker/extension/script.js
@@ -13,57 +13,56 @@
 // limitations under the License.
 
 
-// The port for communicating back to the extension.
-var benchmarkExtensionPort = chrome.extension.connect();
-
 // The url is what this page is known to the benchmark as.
 // The benchmark uses this id to differentiate the benchmark's
 // results from random pages being browsed.
-
 // TODO(mbelshe): If the page redirects, the location changed and the
 // benchmark stalls.
 var benchmarkExtensionUrl = window.location.toString();
-var kTCPReadBytes = "tcp.read_bytes";
-var bytes_read = chrome.benchmarking.counter(kTCPReadBytes);
-var heartbeat_count = 0;
+var heartbeatCount = 0;
+var heartbeatInterval = 200;
+var windowLoad = 0;
+var lastElementLoad = 0;
+var latestLoad = 0;
 
-console.log("script injected into: " + window.location.toString())
+var checkForLastLoad = function() {
+  var benchmarkExtensionPort = chrome.extension.connect();
+  if (lastElementLoad > latestLoad) {
+    latestLoad = lastElementLoad;
+    benchmarkExtensionPort.postMessage({message: 'heartbeat',
+                                        count: heartbeatCount});
+    heartbeatCount++;
+    setTimeout(checkForLastLoad, heartbeatInterval);
+  } else {
+    var loadTimes = chrome.loadTimes();
+    loadTimes.lastLoadTime = latestLoad / 1000.0;
+    benchmarkExtensionPort.postMessage({message: 'load',
+                                        url: benchmarkExtensionUrl,
+                                        values: loadTimes });
+  }
+}
 
-function sendTimesToExtension() {
-  console.log("checking for page done");
+var registerListeners = function() {
   if (window.parent != window) {
     console.log("not my page.");
     return;
   }
-  var load_times = window.chrome.loadTimes();
+  
+  // Called when the window loads.
+  // Set a timeout to check for subresource loads.
+  window.addEventListener('load', function(e) {
+    windowLoad = new Date();
+    latestLoad = windowLoad;
+    console.log("Window loaded at " + windowLoad);
+    setTimeout(checkForLastLoad, heartbeatInterval);
+  }, true);
 
-  var new_bytes_read = chrome.benchmarking.counter(kTCPReadBytes);
-
-  // If the load is not finished yet, schedule a timer to check again in a
-  // little bit.
-  if (load_times.finishLoadTime != 0) {
-    // If the network is still reading bytes, let it acquiesce
-    if (bytes_read == new_bytes_read) {
-      benchmarkExtensionPort.postMessage({message: 'load',
-                                          url: benchmarkExtensionUrl,
-                                          values: load_times });
-      console.log("page finished")
-      return;
-    }
-    console.log("page still changing")
-  }
-
-  // We're still waiting.  Check again in a little bit.
-  bytes_read = new_bytes_read;
-  var id = window.setTimeout(sendTimesToExtension, 200);
-  console.log("will try again in 200ms. timer is: " + id)
-
-  // Send a heartbeat to the extension.
-  benchmarkExtensionPort.postMessage({message: 'heartbeat',
-                                      count: heartbeat_count});
-  heartbeat_count++;
+  // Called each time a subresource loads.
+  // Each time we see one, we check again.
+  document.addEventListener('load', function(e) {
+    lastElementLoad = new Date();
+    console.log("Element loaded at " + lastElementLoad);
+  }, true);
 }
 
-// We can't use the onload event because this script runs at document idle,
-// which may run after the onload has completed.
-sendTimesToExtension();
+registerListeners();
