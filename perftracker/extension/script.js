65ca14a51289727331ca9f06997db572491cb68c
tonyg@google.com <tonyg@google.com@164481a5-15cb-f69f-4b93-856c5b7754c5>
2010-12-31 00:56:02 +0000

    Simplify page load detection. Content script is now a dummy listener that is always injected and returns chrome.loadTimes and performance.timing.
    git-svn-id: http://web-page-replay.googlecode.com/svn/trunk@158 164481a5-15cb-f69f-4b93-856c5b7754c5

--- perftracker/extension/script.js
+++ perftracker/extension/script.js
@@ -12,45 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO(mbelshe): Remove all these console.log statements
 
-// The url is what this page is known to the benchmark as.
-// The benchmark uses this id to differentiate the benchmark's
-// results from random pages being browsed.
-// TODO(mbelshe): If the page redirects, the location changed and the
-// benchmark stalls.
-(function() {
-var benchmarkExtensionUrl = window.location.toString();
-var heartbeatInterval = 1000;
-
-function scheduleCheckForLoadFinished() {
-  console.log("scheduleCheckForLoadFinished()");
-  setTimeout(checkForLoadFinished, heartbeatInterval);
-}
-
-function checkForLoadFinished() {
-  console.log("checkForLoadFinished()");
-
-  var benchmarkExtensionPort = chrome.extension.connect();
-  benchmarkExtensionPort.postMessage({message: 'heartbeat'});
-
-  var loadTimes = chrome.loadTimes() || {};
-  var timing = webkitPerformance.timing || {};
-
-  if (!loadTimes.finishLoadTime || !timing.loadEventStart) {
-    scheduleCheckForLoadFinished();
-  } else {
-    // TODO(tonyg): For diagnostics, this currently ignores LLT and uses PLT.
-    loadTimes.lastLoadTime = timing.loadEventStart - timing.navigationStart;
-    benchmarkExtensionPort.postMessage({message: 'load',
-                                        url: benchmarkExtensionUrl,
-                                        values: loadTimes });
-  }
-}
-
-if (window.parent != window) {
-  console.log("Not my page.");
-} else {
-  scheduleCheckForLoadFinished();
-}
-})();
+// Respond to all requests with timing information for this tab.
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+  sendResponse({
+    timing: webkitPerformance.timing,
+    loadTimes: chrome.loadTimes(),
+  });
+});
