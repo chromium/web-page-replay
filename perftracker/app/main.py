288dfa56db20cdf5e9134b50cf8ddc8cfd44ecc1
tonyg@google.com <tonyg@google.com@164481a5-15cb-f69f-4b93-856c5b7754c5>
2011-01-28 00:41:34 +0000

    Filter negative numbers.
    git-svn-id: http://web-page-replay.googlecode.com/svn/trunk@255 164481a5-15cb-f69f-4b93-856c5b7754c5

--- perftracker/app/main.py
+++ perftracker/app/main.py
@@ -27,19 +27,19 @@ from google.appengine.ext import db
 
 def ApplyStatisticsData(request, obj):
     """Applies statistics uploaded via the request to the object."""
-    obj.start_load_time = int(request.get('start_load_time', 0))
-    obj.dns_time = int(request.get('dns_time', 0))
-    obj.connect_time = int(request.get('connect_time', 0))
-    obj.first_byte_time = int(request.get('first_byte_time', 0))
-    obj.last_byte_time = int(request.get('last_byte_time', 0))
-    obj.doc_load_time = int(request.get('doc_load_time', 0))
-    obj.dcl_time = int(request.get('dcl_time', 0))
-    obj.total_time = int(request.get('total_time', 0))
-    obj.num_requests = int(request.get('num_requests', 0))
-    obj.num_connects = int(request.get('num_connects', 0))
-    obj.num_sessions = int(request.get('num_sessions', 0))
-    obj.read_bytes_kb = int(float(request.get('read_bytes_kb', 0)))
-    obj.write_bytes_kb = int(float(request.get('write_bytes_kb', 0)))
+    obj.start_load_time = max(int(request.get('start_load_time', 0)), 0)
+    obj.dns_time = max(int(request.get('dns_time', 0)), 0)
+    obj.connect_time = max(int(request.get('connect_time', 0)), 0)
+    obj.first_byte_time = max(int(request.get('first_byte_time', 0)), 0)
+    obj.last_byte_time = max(int(request.get('last_byte_time', 0)), 0)
+    obj.doc_load_time = max(int(request.get('doc_load_time', 0)), 0)
+    obj.dcl_time = max(int(request.get('dcl_time', 0)), 0)
+    obj.total_time = max(int(request.get('total_time', 0)), 0)
+    obj.num_requests = max(int(request.get('num_requests', 0)), 0)
+    obj.num_connects = max(int(request.get('num_connects', 0)), 0)
+    obj.num_sessions = max(int(request.get('num_sessions', 0)), 0)
+    obj.read_bytes_kb = max(int(float(request.get('read_bytes_kb', 0))), 0)
+    obj.write_bytes_kb = max(int(float(request.get('write_bytes_kb', 0))), 0)
 
 def BandwidthPrettyString(bandwidth_kbps):
     if bandwidth_kbps >= 1000:
